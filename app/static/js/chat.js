cd3077850e468aabfee4d518c61b801ef7044ab9
Nver Khachoyan <khachoyannver@gmail.com>
2024-01-25 22:00:43 -0800

    JS to handle username input

--- app/static/js/chat.js
+++ app/static/js/chat.js
@@ -1,12 +1,25 @@
+// Define elements
 const user_name = document.getElementById("user_name");
 const join_button = document.getElementById("join_button");
 
-let myUserName = "";
-
-user_name.addEventListener("keyup", function () {
-    myUserName = user_name.value;
+// Event Listeners
+join_button.addEventListener("click", function (event) {
+    handle_submit(event);
 });
 
-join_button.addEventListener('click', function () {
-    console.log(myUserName)
-})
+user_name.addEventListener("keypress", function (event) {
+    if (event.key == "Enter") {
+        handle_submit(event);
+    }
+});
+
+// Handlers
+
+function handle_submit(event) {
+    if (user_name.value == "") {
+        alert("Username cannot be empty!");
+    } else {
+        console.log(user_name.value);
+
+    }
+}
